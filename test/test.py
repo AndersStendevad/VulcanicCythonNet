dffb6e48c4e863b276964735e34d82e7b46f26d5
andst <andst@itu.dk>
2020-11-02 18:50:55 +0100

    Just the basic skeleton

---
+++ test/test.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import cython
+import unittest
+
+class TestVulcanicCythonNet(unittest.TestCase):
+
+    def test_test(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()
+
